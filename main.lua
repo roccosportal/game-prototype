3f2789c755e1dab62eda5c9cf8b859eb7742b3ce
rocco <rocco@roccosportal.com>
2014-09-30 22:57:18 +0200

    preparing different game states

--- main.lua
+++ main.lua
@@ -1,15 +1,9 @@
 require("lib/monocle/monocle")
 game = require("src/game/game")
 
-
-
-
-
 function string.starts(String,Start)
    return string.sub(String,1,string.len(Start))==Start
 end
-
-
 
 
 function love.load()
@@ -20,77 +14,26 @@ function love.load()
        debugToggle='.'   
     })
     Monocle.watch("FPS", function() return math.floor(1/love.timer.getDelta()) end)
-		love.physics.setMeter(64)
-    
-	  world = love.physics.newWorld(0, 9.81*64, true)
-	   
-	 	game.contactEventManager.init(world)
-		game.contactEventManager.onBeginContact(beginContact)
-		
-		game.map.create("maps/test", world)
-    
-    -- create player 
-		local px, py = game.map.current:getPlayerStartingPosition()
-		game.camera:setCenter(px, py)
-		game.player.init(px, py, world)
-    Monocle.watch("onGround", function() return tostring(game.player.onGround) end)
-    
-
-		
-		game.overlays.sounds.init()
-	
-		game.overlays.damage.init()
+		game.init()
 end
 
 
 function love.update( dt )
-		world:update(dt)
-		game.map.current:update(dt)
-		game.player.update(dt)  
-		game.overlays.sounds.update(dt)
-		
-		-- try center camera at player
-		local ww = love.graphics.getWidth()
-		local wh = love.graphics.getHeight()
-		local x, y = game.player.getCenter()
-		x =  math.floor(x - ww / 2)
-		y =  math.floor(y - wh / 2)
-
-		game.camera:update(dt, x, y)
-    
-	  game.overlays.damage.update(dt)
-    Monocle.update()
+    game.update(dt)
+    Monocle.update(dt)
 	
 end
 
 function love.draw()
-		love.graphics.setCanvas()
-    
-		game.camera:set()
-    
-		love.graphics.setBackgroundColor(255, 255, 255)
-		love.graphics.clear()
-		game.map.current:draw()
-		game.player.draw()
-		
-    game.camera:unset()
-    
-    game.overlays.sounds.draw()
-		game.overlays.damage.draw()
+    game.draw()
 	  Monocle.draw()
 end
 
 function love.resize(w, h)
-    game.map.current:resize(w, h)
+    game.resize(w,h)
 end
 
-function beginContact(a, b, contact)
-	  -- if player do nothing
-		if a ~= game.player.fixture and b ~= game.player.fixture then
-			-- forward collision to sound visualisations
-			game.overlays.sounds.collision(contact, a, b)
-		end	
-end
+
 
 function love.textinput(t)
     Monocle.textinput(t)
--- src/game/game.lua
+++ src/game/game.lua
@@ -7,4 +7,72 @@ self.contactEventManager = require("src/game/contact-event-manager")
 self.overlays = require("src/game/overlays/overlays")
 self.camera = require("src/game/camera")
 
+
+function self.init()
+    love.physics.setMeter(64)
+    
+    world = love.physics.newWorld(0, 9.81*64, true)
+     
+    self.contactEventManager.init(world)
+    self.contactEventManager.onBeginContact(beginContact)
+    
+    self.map.create("maps/test", world)
+    
+    -- create player 
+    local px, py = self.map.current:getPlayerStartingPosition()
+    self.camera:setCenter(px, py)
+    self.player.init(px, py, world)
+    Monocle.watch("onGround", function() return tostring(game.player.onGround) end)
+    
+    self.overlays.sounds.init()
+  
+    self.overlays.damage.init()
+end
+
+function self.update(dt)
+    world:update(dt)
+    self.map.current:update(dt)
+    self.player.update(dt)  
+    self.overlays.sounds.update(dt)
+    
+    -- try center camera at player
+    local ww = love.graphics.getWidth()
+    local wh = love.graphics.getHeight()
+    local x, y = self.player.getCenter()
+    x =  math.floor(x - ww / 2)
+    y =  math.floor(y - wh / 2)
+
+    self.camera:update(dt, x, y)
+    
+    self.overlays.damage.update(dt)
+end
+
+function self.draw()
+    love.graphics.setCanvas()
+    
+    self.camera:set()
+    
+    love.graphics.setBackgroundColor(255, 255, 255)
+    love.graphics.clear()
+    self.map.current:draw()
+    self.player.draw()
+    
+    self.camera:unset()
+    
+    self.overlays.sounds.draw()
+    self.overlays.damage.draw()
+end 
+
+function self.resize(w, h)
+    self.map.current:resize(w, h)
+end
+
+function beginContact(a, b, contact)
+    -- if player do nothing
+    if a ~= game.player.fixture and b ~= game.player.fixture then
+      -- forward collision to sound visualisations
+      game.overlays.sounds.collision(contact, a, b)
+    end	
+end
+
 return self
