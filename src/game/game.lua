284ac6dae5893d74138cabdc74693e685290bd05
rocco <rocco@roccosportal.com>
2014-09-30 22:26:56 +0200

    namespace has its own module

--- src/game/game.lua
+++ src/game/game.lua
@@ -1,12 +1,10 @@
-self = {}
+local self = {}
 
 require("src/game/map")
 require("src/game/player")
 require("src/game/contact-event-manager")
 
-self.overlays = {}
-self.overlays.sounds = require("src/game/overlays/sounds")
-self.overlays.damage = require("src/game/overlays/damage")
+self.overlays = require("src/game/overlays/overlays")
 self.camera = require("src/game/camera")
 
 return self
---
+++ src/game/overlays/overlays.lua
@@ -0,0 +1,5 @@
+local self = {}
+self.sounds = require("src/game/overlays/sounds")
+self.damage = require("src/game/overlays/damage")
+
+return self
